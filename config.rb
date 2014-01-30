9175131e6170b3e9ca8156864b1d36d17e9300a2
Melissa Keizer <mkeizer@rumblelabs.com>
2014-01-30 17:45:55 +0000

    installed compass, css edits etc

---
+++ config.rb
@@ -0,0 +1,9 @@
+preferred_syntax = :scss
+http_path = '/'
+css_dir = 'css/'
+sass_dir = 'sass/'
+images_dir = 'assets/images'
+javascripts_dir = 'assets/javascript'
+relative_assets = true
+line_comments = true
+# output_style = :compressed
